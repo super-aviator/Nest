0d1ec2fdb1ebc1ad894d6eb37e865a3db12de43c
熊乾坤 <2372389705@qq.com>
2019-04-03 18:55:29 +0800

    mit
    添加.gitignore文件

--- src/main/java/com/xqk/nest/dto/AccountDTO.java
+++
@@ -1,5 +0,0 @@
-package com.xqk.nest.dto;
-
-public interface AccountDTO {
-    int pwCorrect(String username, String password);
-}
--- src/main/java/com/xqk/nest/dto/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.xqk.nest.dto;
-
-import java.io.IOException;
-
-public interface UserDTO {
-    String getUserInfo(long userId) throws IOException;
-    void changeUserStatus(long userId, String status);
-    void changeUserSign(long userId, String sign);
-}
