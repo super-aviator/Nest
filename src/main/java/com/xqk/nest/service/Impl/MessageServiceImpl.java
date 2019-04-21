7541c1d396a8ecd08ed253355e59121b5e3aafe7
熊乾坤 <2372389705@qq.com>
2019-04-21 20:13:39 +0800

    .

--- src/main/java/com/xqk/nest/service/Impl/MessageServiceImpl.java
+++ src/main/java/com/xqk/nest/service/Impl/MessageServiceImpl.java
@@ -1,16 +1,13 @@
 package com.xqk.nest.service.Impl;
 
-import com.xqk.nest.config.MySqlSessionFactory;
 import com.xqk.nest.dao.MessageDAO;
 import com.xqk.nest.dto.*;
 import com.xqk.nest.service.MessageService;
 import com.xqk.nest.websocket.dto.HistoryChatMessageDTO;
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
